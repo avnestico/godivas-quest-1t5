bf2377f71ce981ee85848e6e65905503abecb2ec
avnestico <avnestico@users.noreply.github.com>
2014-11-10 23:00:12 -0500

    Revert relative links in header data - absolute links are necessary

--- php_source/header_data.php
+++ php_source/header_data.php
@@ -1,15 +1,15 @@
 <p class="center"></p>
 
 <h3><b>Menu</b></h3><b>
-    <a href="../index.php">Home</a><br/>
-    <!-- <a href="../phase1/index.php">Phase 1</a><br />
-    <a href="../phase2/index.php">Phase 2</a><br />
-    <a href="../phase3/index.php">Phase 3</a><br /> -->
-    <a href="../info/registration.php">Registration</a><br/>
-    <a href="../info/leaderboard.php">Leaderboard</a><br/>
-    <a href="../info/history.php">History</a><br/>
-    <a href="../info/hints.php">Hints</a><br/></b><br/>
-    <!-- <a href="../info/winners.php">Winners</a><br /> -->
+    <a href="/1T5/index.php">Home</a><br/>
+    <!-- <a href="/1T5/phase1/index.php">Phase 1</a><br />
+    <a href="/1T5/phase2/index.php">Phase 2</a><br />
+    <a href="/1T5/phase3/index.php">Phase 3</a><br /> -->
+    <a href="/1T5/info/registration.php">Registration</a><br/>
+    <a href="/1T5/info/leaderboard.php">Leaderboard</a><br/>
+    <a href="/1T5/info/history.php">History</a><br/>
+    <a href="/1T5/info/hints.php">Hints</a><br/></b><br/>
+<!-- <a href="/1T5/info/winners.php">Winners</a><br /> -->
 
 <h3>Previous Quests</h3>
 
