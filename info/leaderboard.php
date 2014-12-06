544b425e8a244c0ec1957ad8fcd7978be4446f2a
avnestico <avnestico@users.noreply.github.com>
2014-12-06 01:25:34 -0500

    Comment explaining count_num_solved

--- info/leaderboard.php
+++ info/leaderboard.php
@@ -72,6 +72,7 @@ function print_leaderboard_header($fieldCount)
                 $num_solved = 0;
                 list($id, $full_name, $alias, $email) = get_info_from_row($row);
                 echo "<tr><td><div>$full_name</div></td>";
+                // Returns the number of cells with 'Y' in them. If the 3rd argument is set to true, prints cells in table.
                 $num_solved = count_num_solved($field_count, $row, true);
                 echo "<td><div style='width:48px'>" . $row["last_solve"] . "</div></td><td><div id='small_cell'>$num_solved</div></td></tr>";
             }
