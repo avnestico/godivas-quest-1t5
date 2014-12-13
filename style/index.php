3914b27978b906af12d4b491c5366df8351795e0
avnestico <avnestico@users.noreply.github.com>
2014-12-12 21:14:07 -0500

    Add style index page with note for user to come back later

---
+++ style/index.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+include_once(__DIR__ . "/../php_source/global_variables.php");
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta name="generator" content=
+    "HTML Tidy for Linux/x86 (vers 25 March 2009), see www.w3.org"/>
+    <meta name="viewport" content="width=800px"/>
+
+    <title>Style</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../favicon.ico"/>
+    <link rel="stylesheet" href="../style/mystyle.css" type="text/css" media="screen"/>
+</head>
+
+<body>
+<div id="container">
+    <?php
+    include_once(__DIR__ . "/../php_source/header.php");
+    include_once(__DIR__ . "/../php_source/message.php");
+    ?>
+
+    <div id="content_container">
+        <div id="content">
+            <h2>Style</h2>
+
+            <p>
+                Come back here when the Quest begins.
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
