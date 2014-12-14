c85c95b5b71689b0ed6a6b1d28285e0f3e1773de
avnestico <avnestico@users.noreply.github.com>
2014-12-14 13:22:52 -0500

    Add phase 3 index page

---
+++ phase3/index.php
@@ -0,0 +1,46 @@
+<?php
+$questionNumber = 21;
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta name="generator" content=
+    "HTML Tidy for Linux/x86 (vers 25 March 2009), see www.w3.org"/>
+    <meta name="viewport" content="width=800px"/>
+
+    <title>Game, Set, Match</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../favicon.ico"/>
+    <link rel="stylesheet" href="../style/mystyle.css" type="text/css"/>
+</head>
+<body>
+<div id="container">
+    <?php
+    include_once(__DIR__ . "/../php_source/puzzles/puzzle_header.php");
+    include_once(__DIR__ . "/../php_source/message.php");
+    ?>
+    <div id="content_container">
+        <div id="content">
+            <h2>The end of the Quest is upon us!</h2>
+
+            <p>All of the Quest is now open! If I were you, I'd get a move on, because only the first person to
+                complete the Quest can be named Godiva's Quest Champion!</p>
+
+            <p>Ms. Bold,<br/>
+                Godiva Industries President
+            </p>
+        </div>
+    </div>
+
+    <div id="content_container">
+        <div id="content">
+            <h2>Game, Set< Match</h2>
+
+
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
