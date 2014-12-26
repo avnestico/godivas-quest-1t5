97f50cd0c875213aca5ca80b6170656c9b4ac25d
avnestico <avnestico@users.noreply.github.com>
2014-12-26 14:32:03 -0500

    Add functions to change single elements of SQL tables

---
+++ php_source/backup/change_user_info.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once(__DIR__ . "/../quest_db.php");
+
+// If the number of arguments is incorrect, cancel change.
+if ($argc != 5) {
+    die("Usage: php -f change_user_info.php <SQL table> { alias | email | first_name | last_name } <old value> <new value>\n");
+}
+
+$table = $argv[1];
+$column = $argv[2];
+$old_value = $argv[3];
+$new_value = $argv[4];
+
+$query = check_for_existence($table, $column, $old_value);
+
+if ($query->rowCount()) {
+    change_instances($table, $column, $old_value, $new_value);
+    echo "Change made successfully.\n";
+} else {
+    echo "No instances found.\n";
+}
