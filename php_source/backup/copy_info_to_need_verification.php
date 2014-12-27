f778e8bf9aa2a030e8ed338640626790201035a4
avnestico <avnestico@users.noreply.github.com>
2014-12-26 21:21:50 -0500

    Modify copy_info to be able to move single users

--- php_source/backup/copy_info_to_need_verification.php
+++ php_source/backup/copy_info_to_need_verification.php
@@ -10,9 +10,7 @@ include_once(__DIR__ . "/../global_functions.php");
 require_once(__DIR__ . "/../quest_db.php");
 include_once(__DIR__ . "/../registration/verification_functions.php");
 
-$query = select_all_aliases("backup");
-
-while ($row = $query->fetch()) {
+function copy_info($row) {
     $email = $row['email'];
     $first_name = $row['first_name'];
     $last_name = $row['last_name'];
@@ -23,7 +21,7 @@ while ($row = $query->fetch()) {
     if ($check_query->rowCount()) {
         echo "User already exists: $email\n";
     } else {
-        if (strpos($email,'utoronto.ca') !== false) {
+        if (strpos($email, 'utoronto.ca') !== false) {
             $alias_hash = get_verification_url($alias);
             register_user($first_name, $last_name, $alias, $alias_hash, $email);
             email_register($first_name, $alias_hash, $email);
@@ -32,5 +30,25 @@ while ($row = $query->fetch()) {
             email_not_registered($first_name, $email);
             echo "Information email sent: $email\n";
         }
+    }
+}
+
+if ($argc == 1) {
+    $query = select_all_aliases("backup");
+
+    while ($row = $query->fetch()) {
+        copy_info($row);
+    }
+}
+
+if ($argc == 2) {
+    $alias = $argv[1];
+    $single_user_query = check_for_existence("backup", "alias", $alias);
+
+    if ($single_user_query->rowCount()) {
+        $row = $single_user_query->fetch();
+        copy_info($row);
+    } else {
+        echo "Alias does not exist.\n";
     }
 }
