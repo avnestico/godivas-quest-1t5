7ba77eb4f61d4c08965437385b131b593c152e35
avnestico <avnestico@users.noreply.github.com>
2015-01-11 01:19:42 -0500

    Pass puzzle solution to template as optional arg

--- php_source/puzzles/puzzle_template.php
+++ php_source/puzzles/puzzle_template.php
@@ -7,12 +7,13 @@ include_once(__DIR__ . "/../global_variables.php");
  * Print formatted html with puzzle template.
  *
  * Copy everything in the next comment into a new file, changing the location of the require_once if necessary, and add
- * the page's question, title, and content. If printing a solution page, add true as a fourth optional argument.
+ * the page's question, title, and content. If printing a solution page, enter the puzzle's solution as a fourth
+ * argument. This adds a sidebar link to return to the puzzle (assuming the solution is in a subfolder of the puzzle)
  *
  * @param $questionNumber
  * @param $questionTitle
  * @param $content
- * @param bool $is_solution
+ * @param string $solution: The puzzle's solution. Optional arg; when included, the template prints a solution page.
  */
 
 /*
@@ -27,11 +28,11 @@ ob_start();
 // content of page goes here
 
 <?php
-print_template($questionNumber, $questionTitle, ob_get_clean()); // Add true to print solution template
+print_template($questionNumber, $questionTitle, ob_get_clean()); // Add puzzle's solution as fourth arg to print solution page
 
 */
 
-function print_template($questionNumber, $questionTitle, $content, $is_solution = false) {
+function print_template($questionNumber, $questionTitle, $content, $solution = "") {
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -49,21 +50,21 @@ function print_template($questionNumber, $questionTitle, $content, $is_solution 
     <div id="container">
         <?php
         include_once(__DIR__ . "/puzzle_header.php");
-        print_puzzle_header($is_solution);
+        print_puzzle_header($solution); // Casts to bool. False if empty (default behaviour), true otherwise.
         include_once(__DIR__ . "/../message.php");
         ?>
         <div id="content_container">
             <div id="content">
                 <h2>
-                    <?php if ($is_solution) echo('Solution to ') ?>#<?php echo $questionNumber ?>: <?php echo $questionTitle ?>
+                    <?php if ($solution) echo('Solution to ') ?>#<?php echo $questionNumber ?>: <?php echo $questionTitle ?>
                 </h2>
                 <?php
-                if ($is_solution) {
+                if ($solution) {
                     echo('<pre><code>');
                 }
                 echo($content);
-                if ($is_solution) {
-                    echo('</code></pre>');
+                if ($solution) {
+                    echo('<div id="solution">' . $solution . '</div></code></pre>');
                 }
                 ?>
             </div>
