723e172164b9676a2155acd78beb15876c58bfab
avnestico <avnestico@users.noreply.github.com>
2015-01-11 01:35:26 -0500

    fix wording of puzzle 2 solution

--- phase1/startups/solution/index.php
+++ phase1/startups/solution/index.php
@@ -7,7 +7,7 @@ ob_start();
 ?>
 The seven companies, in the order listed, are:
 
-FORTUNE BRANDS
+FORTUNEBRANDS
 COLECO
 YOUTUBE
 AVONPRODUCTS
@@ -15,7 +15,7 @@ JOHNDEERE
 WRIGLEY
 NINTENDO
 
-As hinted by '45 degree angle', you want to look at the first letter of the first answer, second letter of the second answer, and so on. Doing so spells 'founded', the puzzle's answer.
+As hinted by '45 degree angle', you want to look at the first letter of the first answer, second letter of the second answer, and so on. Doing so spells 'founded', the puzzle's solution.
 
 <?php
 print_template($questionNumber, $questionTitle, ob_get_clean(), "FOUNDED");
