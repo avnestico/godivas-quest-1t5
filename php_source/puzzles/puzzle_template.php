2d161ad4208bda485eb66943a14273ccc70c272e
avnestico <avnestico@users.noreply.github.com>
2015-01-10 20:58:08 -0500

    Tweak solution template

--- php_source/puzzles/puzzle_template.php
+++ php_source/puzzles/puzzle_template.php
@@ -1,4 +1,6 @@
 <?php
+include_once(__DIR__ . "/../global_variables.php");
+
 /**
  * print_template
  *
@@ -10,7 +12,7 @@
  * @param $questionNumber
  * @param $questionTitle
  * @param $content
- * @param bool $solution
+ * @param bool $is_solution
  */
 
 /*
@@ -39,8 +41,8 @@ function print_template($questionNumber, $questionTitle, $content, $is_solution 
     "HTML Tidy for Linux/x86 (vers 25 March 2009), see www.w3.org"/>
     <meta name="viewport" content="width=800px"/>
     <title><?php echo $questionTitle ?></title>
-    <link rel="shortcut icon" type="image/x-icon" href="../../favicon.ico"/>
-    <link rel="stylesheet" href="../../style/mystyle.css" type=
+    <link rel="shortcut icon" type="image/x-icon" href="/<?php echo $GLOBALS['this_year'] ?>/favicon.ico"/>
+    <link rel="stylesheet" href="/<?php echo $GLOBALS['this_year'] ?>/style/mystyle.css" type=
     "text/css"/>
 </head>
 <body>
@@ -51,15 +53,19 @@ function print_template($questionNumber, $questionTitle, $content, $is_solution 
         include_once(__DIR__ . "/../message.php");
         ?>
         <div id="content_container">
-            <?php
-            if (!$is_solution) {
-                echo('<div id="content">');
-            } else {
-                echo('<div id="solution">');
-            }
-            ?>
-                <h2><?php if ($is_solution) echo('Solution to ') ?>#<?php echo $questionNumber ?>: <?php echo $questionTitle ?></h2>
-                <?php echo $content ?>
+            <div id="content">
+                <h2>
+                    <?php if ($is_solution) echo('Solution to ') ?>#<?php echo $questionNumber ?>: <?php echo $questionTitle ?>
+                </h2>
+                <?php
+                if ($is_solution) {
+                    echo('<div id="solution">');
+                }
+                echo($content);
+                if ($is_solution) {
+                    echo('</div>');
+                }
+                ?>
             </div>
         </div>
     </div>
