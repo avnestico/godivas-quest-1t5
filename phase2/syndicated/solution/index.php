608dea1d655ea9b58f730f2351ae8c8cedaa4afd
avnestico <avnestico@users.noreply.github.com>
2015-01-14 23:32:13 -0500

    Full solution for puzzle 12

--- phase2/syndicated/solution/index.php
+++ phase2/syndicated/solution/index.php
@@ -1,5 +1,7 @@
+<script src="../../../php_source/js/toggle_visibility.js" type="text/javascript"></script>
 <?php
 require_once(__DIR__ . "/../../../php_source/puzzles/puzzle_template.php");
+require_once(__DIR__ . "/../../../php_source/global_functions.php");
 
 $questionNumber = 12;
 $questionTitle = "Syndicated";
@@ -20,10 +22,71 @@ It becomes quite clear that this puzzle isn't an ordinary Challenger. All the nu
 |----|----|----|----#====#
 |    | 14 |    |    # 21 #
 #====#====#====#====#====#
-# 23 # 30 # 31 # 38 # 35 #
+# 23 # 30 # 31 # 28 # 35 #
 #====#====#====#====#====#
 
-There is only one solution to this Challenger in which every number is between 1 and 15. With patience, it can be found without needing to make any guesses. The solution is:
+There is only one solution to this Challenger in which every number is between 1 and 15. With patience, it can be found without needing to make any guesses. The solution is as follows:
+
+<?php print_spoiler("spoiler", "Click here to expand puzzle solution", true) ?>
+#====#====#====#====#====#====#
+# (\)# (A)# (B)# (C)# (D)# 19 #
+#====#====#====#====#====#====#
+# (1)#  3 |    |    |    # 32 #
+#====#----|----|----|----#====#
+# (2)#    |    |  2 |    # 37 #
+#====#----|----|----|----#====#
+# (3)#  3 |    |    |  4 # 22 #
+#====#----|----|----|----#====#
+# (4)#    | 14 |    |    # 21 #
+#====#====#====#====#====#====#
+# (/)# 23 # 30 # 31 # 28 # 35 #
+#====#====#====#====#====#====#
+
+(4): (4A) + (4C) + (4D) = 7     Each cell can be no larger than 5
+
+(A): (2A) + (4A) = 17           The largest a cell can be is 15
+                                Thus, 2 &lt;= (4A) &lt;= 5
+                                Thus, (4C) and (4D) are each &lt;= 4
+
+(\): (2B) + (3C) + (4D) = 32    The largest a cell can be is 15
+                                Thus, 2 &lt;= (4D) &lt;= 4
+                                Thus, (2B) and (3C) are each &gt;= 13
+                                Thus, 2 &lt;= (4A) &lt;= 4 and 1 &lt;= (4C) &lt; 3
+
+(B): (1B) + (2B) + (3B) = 16    The smallest a cell can be is 1
+                                Thus, 13 &lt;= (2B) &lt;= 14
+
+(3): (3B) + (3C) = 15           The smallest a cell can be is 1
+                                Thus, 13 &lt;= (3C) &lt;= 14
+
+(\): (2B) + (3C) + (4D) = 32    Note (2B) &lt;= 14; (3C) &lt;= 14; (4D) &lt;= 4
+                                Thus (2B) = (3C) = 14 and (4D) = 4
+
+#====#====#====#====#====#====#
+# (\)# (A)# (B)# (C)# (D)# 19 #
+#====#====#====#====#====#====#
+# (1)#  3 |    |    |    # 32 #
+#====#----|----|----|----#====#
+# (2)#    | 14 |  2 |    # 37 #
+#====#----|----|----|----#====#
+# (3)#  3 |    | 14 |  4 # 22 #
+#====#----|----|----|----#====#
+# (4)#    | 14 |    |  4 # 21 #
+#====#====#====#====#====#====#
+# (/)# 23 # 30 # 31 # 28 # 35 #
+#====#====#====#====#====#====#
+
+The rest of the puzzle can be trivially solved because we know (4A) &gt; 2
+
+(4): (4A) = 2, (4B) = 1
+(A): (2A) = 15
+(C): (1C) = 14
+(3): (3B) = 1
+(/): (1D) = 14
+(B): (1B) = 1
+(1): (1D) = 14
+<?php print_spoiler_end() ?>
+The solution looks like this:
 
                     #====#
                     # 19 #
@@ -36,7 +99,7 @@ There is only one solution to this Challenger in which every number is between 1
 |----|----|----|----#====#
 |  2 | 14 |  1 |  4 # 21 #
 #====#====#====#====#====#
-# 23 # 30 # 31 # 38 # 35 #
+# 23 # 30 # 31 # 28 # 35 #
 #====#====#====#====#====#
 
 Reading the box from left to right, top to bottom, you get the hex string 31EEFE2631E42E14.
